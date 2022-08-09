d679cb4a35434bca221505abf2ecc797d5fb0785
OfficialMaritza <101466695+OfficialMaritza@users.noreply.github.com>
2022-08-09 18:39:32 +0100

    Add files via upload

---
+++ Shop.js
@@ -0,0 +1,21 @@
+
+//ADD YOUR FIREBASE LINKS
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD0TsQvXuLg6Cj6eHQgXm_7FyxG7vG3fcI",
+  authDomain: "little-mammals.firebaseapp.com",
+  projectId: "little-mammals",
+  storageBucket: "little-mammals.appspot.com",
+  messagingSenderId: "201505865206",
+  appId: "1:201505865206:web:019691f8105fbb18391bd7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+function AddToCart() {
+  
+    window.location = "Boughtsmth.html";
+}
+
